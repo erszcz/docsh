1fcf2a720b67f22e13b40e4a2a0db6f51f7be775
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-23 00:09:04 +0100

    Tune snippets.erl for more convenient copy-pasting

--- snippets.erl
+++ snippets.erl
@@ -1,6 +1,7 @@
-{ok, {_, C}} = beam_lib:chunks("/Users/erszcz/apps/elixir/xyz/bin/../lib/elixir/ebin/Elixir.Agent.beam", ["ExDc"]).
+P = "/home/erszcz/apps/elixir/1.2.1/bin/../lib/elixir/ebin/Elixir.Agent.beam".
+{ok, {_, C}} = beam_lib:chunks(P, ["ExDc"]).
 [{_, B}] = C.
-DocsV1 = binary_to_term(B).
+Docs = binary_to_term(B).
 {_ModLineNo, ModDoc} = proplists:get_value(moduledoc, element(2, DocsV1)).
 FunDocs = proplists:get_value(docs, element(2, DocsV1)).
 {{update,3}, _FunLineNo, _Type, _Something, FunDoc} = lists:keyfind({update,3}, 1, FunDocs).
