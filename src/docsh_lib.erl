2c32efc8df9ec20c19b0da85833eac7b118d4926
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-23 00:09:54 +0100

    Convert internal format to something resembling elixir_docs_v1

--- src/docsh_lib.erl
+++ src/docsh_lib.erl
@@ -1,6 +1,13 @@
 -module(docsh_lib).
 
--export([print/2]).
+-export([get/2,
+	 print/2]).
+
+get(Key, Props) ->
+    case lists:keyfind(Key, 1, Props) of
+	false -> error(not_found, [Key, Props]);
+	{Key, Val} -> Val
+    end.
 
 print(Fmt, Args) ->
     io:format(Fmt, Args).
