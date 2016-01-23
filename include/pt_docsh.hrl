eef4ef94da111c9bb8bbd1f48b4e23ddfac98913
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-23 16:56:00 +0100

    Export generate Mod:h/2 for function doc access
    > recon:h(info, 3).
    Equivalent to info(<A.B.C>) where A, B, and C are integers part
      of a pid
    ok

--- include/pt_docsh.hrl
+++ include/pt_docsh.hrl
@@ -4,4 +4,4 @@
 %%       but somehow it doesn't work,
 %%       i.e. breaks the compiler with {1,erl_lint,{undefined_function,[{h,0}]}},
 %%       though h/0 is also generated at compile time and embedded in the module
--export([h/0]).
+-export([h/0, h/2]).
