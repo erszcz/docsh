11c5741f4372c707c0ea13af35be9e3dfda2dd0f
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-23 23:36:00 +0100

    Add some snippets

--- snippets.erl
+++ snippets.erl
@@ -5,3 +5,33 @@ Docs = binary_to_term(B).
 {_ModLineNo, ModDoc} = proplists:get_value(moduledoc, element(2, DocsV1)).
 FunDocs = proplists:get_value(docs, element(2, DocsV1)).
 {{update,3}, _FunLineNo, _Type, _Something, FunDoc} = lists:keyfind({update,3}, 1, FunDocs).
+
+S3 = "/home/erszcz/work/lavrin/docsh/test/edoc_example.erl".
+docsh_edoc:to_internal(S3).
+
+S3 = "/home/erszcz/work/lavrin/docsh/test/edoc_example.erl".
+{ok, BData} = file:read_file(S3).
+{ok, Tokens, _} = erl_scan:string(binary_to_list(BData)).
+%erl_parse:parse_exprs(Tokens).
+
+%erl_scan:string(Str)
+%Dot = {dot, erl_anno:new(1)},
+%erl_parse:parse_term(Tokens ++ [Dot])
+
+Spec = {attribute,13,spec,
+        {{f,0},
+         [{type,13,'fun',
+           [{type,13,product,[]},{user_type,13,r,[]}]}]}}.
+iolist_to_binary(erl_pp:form(Spec)).
+% <<"-spec f() -> r().\n">>
+
+f().
+Spec = {attribute,17,spec,
+        {{g,0},
+         [{type,17,bounded_fun,
+           [{type,17,'fun',[{type,17,product,[]},{var,17,'R'}]},
+            [{type,18,constraint,
+              [{atom,18,is_subtype},
+               [{var,18,'R'},{user_type,18,r,[]}]]}]]}]}}.
+iolist_to_binary(erl_pp:form(Spec)).
+% <<"-spec g() -> R when is_subtype(R, r()).\n">>
