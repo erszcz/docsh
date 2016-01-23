20907e833d72dd82b34f65b0a6ab93a4ffd68ad6
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-23 13:04:14 +0100

    Remove debugging printouts

--- src/docsh_edoc.erl
+++ src/docsh_edoc.erl
@@ -2,15 +2,9 @@
 
 -export([to_internal/1]).
 
--import(docsh_lib, [print/2]).
-
 to_internal(File) ->
     xmerl:export_simple([edoc(File)], docsh_edoc_xmerl).
 
 edoc(File) ->
     {_Mod, EDoc} = edoc:get_doc(File, []),
-    print("edoc xml: ~s~n", [xml(EDoc)]),
     EDoc.
-
-xml(EDoc) ->
-    iolist_to_binary(xmerl:export_simple([EDoc], xmerl_xml)).
