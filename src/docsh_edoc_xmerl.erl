2e5c21673b05373bfb948feb7b5ab585f9952e16
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-23 12:43:57 +0100

    Extend edoc_xmerl extractor to make recon.erl pass the transformation

--- src/docsh_edoc_xmerl.erl
+++ src/docsh_edoc_xmerl.erl
@@ -1,32 +1,61 @@
 -module(docsh_edoc_xmerl).
 
 -export(['#root#'/4,
-	 '#element#'/5,
-	 '#text#'/1,
-	 '#xml-inheritance#'/0]).
+         '#element#'/5,
+         '#text#'/1,
+         '#xml-inheritance#'/0]).
+
+-import(docsh_lib, [print/2]).
 
 -include_lib("xmerl/include/xmerl.hrl").
 
+-define(il2b(IOList), iolist_to_binary(IOList)).
 -define(l2b(L), list_to_binary(L)).
 -define(l2i(L), list_to_integer(L)).
 -define(l2ea(L), list_to_existing_atom(L)).
 
 %% The '#text#' function is called for every text segment.
-'#text#'(Text) -> iolist_to_binary(Text).
+'#text#'(Text) -> ?il2b(Text).
 
 %% The '#root#' tag is called when the entire structure has been
 %% exported. It does not appear in the structure itself.
-'#root#'([{module, Data, Attrs}], _Attrs, [], _E) ->
-    {functions, Functions, _} = lists:keyfind(functions, 1, Data),
-    [{module, (module_details_from_attrs(Attrs) ++
-	       module_details_from_data(Data))}] ++ Functions.
+'#root#'(Data, _Attrs, [], _E) ->
+    lists:flatten(Data).
 
 %% The '#element#' function is the default handler for XML elements.
 '#element#'(function, Data, Attrs, _Parents, _E) ->
-    {function, (function_details_from_attrs(Attrs) ++
-	        function_details_from_data(Data))};
-'#element#'(Tag, Data, Attrs, _Parents, _E) ->
-    {Tag, Data, Attrs}.
+    [{function, (function_details_from_attrs(Attrs) ++
+                 function_details_from_data(Data))}];
+'#element#'(equiv, Data, _Attrs, _Parents, _E) ->
+    {see, [Reference]} = lists:keyfind(see, 1, lists:flatten(Data)),
+    [{description, ?il2b([<<"See ">>, Reference])}];
+'#element#'(functions, Data, _Attrs, _Parents, _E) ->
+    %% Functions are already extracted.
+    [{functions, Data}];
+'#element#'(module, Data, Attrs, _Parents, _E) ->
+    Details = module_details_from_data(Data),
+    {value, {functions, Functions}, Rest} = lists:keytake(functions, 1, Details),
+    [{module, module_details_from_attrs(Attrs) ++ Rest}
+     | Functions];
+'#element#'(briefDescription, _Data, _Attrs, _Parents, _E) ->
+    [];
+'#element#'(description, Data, _Attrs, _Parents, _E) ->
+    %% Flatten all text content from child elements.
+    [{description, ?il2b(Data)}];
+'#element#'(Tag, Data, _Attrs, _Parents, _E)
+        when Tag =:= a;
+             Tag =:= code;
+             Tag =:= dd;
+             Tag =:= dl;
+             Tag =:= dt;
+             Tag =:= fullDescription;
+             Tag =:= li;
+             Tag =:= p;
+             Tag =:= ul ->
+    %% Just get the text.
+    Data;
+'#element#'(Tag, Data, _Attrs, _Parents, _E) ->
+    [{Tag, Data}].
 
 %% Unused.
 '#xml-inheritance#'() -> [].
@@ -40,12 +69,13 @@ module_detail_from_attr(#xmlAttribute{}) ->
     ignore.
 
 module_details_from_data(Data) ->
-    [ D || Datum <- Data, D <- [module_detail_from_data(Datum)], D /= ignore ].
+    lists:flatmap(fun module_detail_from_data/1, Data).
 
-module_detail_from_data({description, _, _} = Desc) ->
-    description(Desc);
-module_detail_from_data(_) ->
-    ignore.
+module_detail_from_data([{description, _}] = Desc) ->
+    Desc;
+module_detail_from_data([{functions, _}] = Functions) ->
+    Functions;
+module_detail_from_data(_) -> [].
 
 function_details_from_attrs(Attrs) ->
     [ function_detail(At) || At <- Attrs ].
@@ -60,16 +90,11 @@ function_detail(#xmlAttribute{name = label} = At) ->
     {label, ?l2b(value(At))}.
 
 function_details_from_data(Data) ->
-    [ D || Datum <- Data, D <- [function_detail_from_data(Datum)], D /= ignore ].
+    lists:flatmap(fun function_detail_from_data/1, Data).
 
-function_detail_from_data({description, _, _} = Desc) ->
-    description(Desc);
-function_detail_from_data(_) ->
-    ignore.
-
-description({description, Desc, _}) ->
-    [{briefDescription, _, _}, {fullDescription, [Full], _}] = Desc,
-    {description, Full}.
+function_detail_from_data([{description, _}] = Desc) ->
+    Desc;
+function_detail_from_data(_) -> [].
 
 value(#xmlAttribute{value = V}) -> V.
 
