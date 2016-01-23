dd079b590c86d196be6dac463750e4ce9747ec92
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-23 20:18:26 +0100

    Don't use tabs for indentation

--- src/docsh_lib.erl
+++ src/docsh_lib.erl
@@ -1,12 +1,12 @@
 -module(docsh_lib).
 
 -export([get/2,
-	 print/2]).
+         print/2]).
 
 get(Key, Props) ->
     case lists:keyfind(Key, 1, Props) of
-	false -> error(not_found, [Key, Props]);
-	{Key, Val} -> Val
+        false -> error(not_found, [Key, Props]);
+        {Key, Val} -> Val
     end.
 
 print(Fmt, Args) ->
