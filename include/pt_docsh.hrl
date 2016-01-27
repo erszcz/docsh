803925e88b7833d5ef6342fae9cd70e2663735b5
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-27 23:19:41 +0100

    Export h/0,2 programatically
    The -export() attribute in pt_docsh.hrl made it impossible to generate
    embedded documentation for a whole project with just a single directive
    in rebar.config, as the generated helpers would not be exported.
    Now, when the export attribute is generated by the transformation
    this is no longer the case and {parse_transform, pt_docsh} in
    rebar.config is sufficient to build docs for the whole project.

--- include/pt_docsh.hrl
+++ include/pt_docsh.hrl
@@ -1,7 +1,1 @@
 -compile([{parse_transform, pt_docsh}]).
-
-%% TODO: this could be added at compile time by pt_docsh,
-%%       but somehow it doesn't work,
-%%       i.e. breaks the compiler with {1,erl_lint,{undefined_function,[{h,0}]}},
-%%       though h/0 is also generated at compile time and embedded in the module
--export([h/0, h/2]).
