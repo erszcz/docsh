4ec69d72584244263067e1684dc870ee16a94e07
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-29 20:46:40 +0100

    Extend edoc_example.erl docs with more complex markup

--- test/edoc_example.erl
+++ test/edoc_example.erl
@@ -1,7 +1,8 @@
 %% @doc Top-level module doc.
 %% @end
 -module(edoc_example).
--export([f/0]).
+-export([f/0,
+         g/0]).
 
 -include_lib("docsh/include/pt_docsh.hrl").
 
@@ -12,3 +13,37 @@
 -spec f() -> r().
 f() ->
     ok.
+
+%% @doc g() returns a more complex value,
+%% while its documentation uses more complex markup.
+%%
+%% <dt>
+%%     <dt>Why?</dt>
+%%     <dd>To test the documentation extraction and formatting process.</dd>
+%%
+%%     <dt>Any other reason?</dt>
+%%     <dd>Not really.</dd>
+%% </dt>
+%%
+%% <ul>
+%%     <li>Some</li>
+%%     <li>Random</li>
+%%     <li>Items</li>
+%% </ul>
+%%
+%% <ol>
+%%     <li>One</li>
+%%     <li>Two</li>
+%%     <li>
+%%         <p>Three</p>
+%%         <ul>
+%%          <li>a</li>
+%%          <li>b</li>
+%%          <li>c</li>
+%%         </ul>
+%%     </li>
+%% </ol>
+%% @end
+g() ->
+    Value = fun f/0,
+    {some, [more, <<"complex">>, Value]}.
