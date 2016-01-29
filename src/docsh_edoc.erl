d1f80d8d35e980a3fa718026874f05d82a84f82b
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-29 21:12:02 +0100

    Add conditional lazily-evaluated debugging

--- src/docsh_edoc.erl
+++ src/docsh_edoc.erl
@@ -3,9 +3,16 @@
 
 -export([to_internal/1]).
 
+-import(docsh_lib, [debug/3]).
+
+-define(l(Args), fun () -> Args end).
+
 -spec to_internal(file:filename()) -> docsh:internal().
 to_internal(File) ->
-    xmerl:export_simple([edoc(File)], docsh_edoc_xmerl).
+    EDoc = edoc(File),
+    debug(edoc, "edoc:~n~p~n~n", ?l([EDoc])),
+    debug(xml,  "xml:~n~s~n~n",  ?l([xmerl:export_simple([EDoc], xmerl_xml)])),
+    xmerl:export_simple([EDoc], docsh_edoc_xmerl).
 
 edoc(File) ->
     {_Mod, EDoc} = edoc:get_doc(File, []),
--- src/docsh_lib.erl
+++ src/docsh_lib.erl
@@ -1,6 +1,7 @@
 -module(docsh_lib).
 
 -export([get/2, get/3,
+         debug/3,
          print/2]).
 
 -type k() :: any().
@@ -24,3 +25,20 @@ get(Key, Props, Default) ->
 -spec print(io:format(), [term()]) -> ok.
 print(Fmt, Args) ->
     io:format(Fmt, Args).
+
+-spec debug(atom(), io:format(), [term()] | fun(() -> [term()])) -> ok.
+debug(Tag, Fmt, Args) ->
+    case os:getenv("DOCSH_DEBUG") of
+        false -> ok;
+        Tags -> debug_matching(string:tokens(Tags, ","),
+                               atom_to_list(Tag), Fmt, Args)
+    end.
+
+debug_matching(Tags, Tag, Fmt, Args) ->
+    case lists:member(Tag, Tags) of
+        false -> ok;
+        true -> print(Fmt, if
+                               is_function(Args, 0) -> Args();
+                               is_list(Args) -> Args
+                           end)
+    end.
