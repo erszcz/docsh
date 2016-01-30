b15947cde96d3f7b247fe11dda6cae7bf9c2fa2c
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-30 22:32:23 +0100

    Test ul formatting (test: red)

--- test/edoc_example2.erl
+++ test/edoc_example2.erl
@@ -6,7 +6,8 @@
          p/0,
          pre/0,
          text1/0,
-         text2/0]).
+         text2/0,
+         ul/0]).
 
 -include_lib("docsh/include/pt_docsh.hrl").
 
@@ -84,3 +85,18 @@ text2() -> ok.
 %% it dynamically on older versions of OTP.
 -spec code_in_loose_text() -> ok.
 code_in_loose_text() -> ok.
+
+%% @doc
+%% <ul>
+%%  <li>`Module' is any atom representing a module</li>
+%%  <li>`Function' is any atom representing a function, or the wildcard
+%%      <code>'_'</code></li>
+%%  <li>`Args' is either the arity of a function (`0..255'), a wildcard
+%%      pattern (<code>'_'</code>), a
+%%      <a href="http://learnyousomeerlang.com/ets#you-have-been-selected">match specification</a>,
+%%      or a function from a shell session that can be transformed into
+%%      a match specification</li>
+%% </ul>
+%% @end
+-spec ul() -> ok.
+ul() -> ok.
