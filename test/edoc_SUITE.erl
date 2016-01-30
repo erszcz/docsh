b469e355ea7204e026b622a28587aaa3cd174722
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-30 15:31:27 +0100

    wip: Test EDoc formatting

--- test/edoc_SUITE.erl
+++ test/edoc_SUITE.erl
@@ -6,13 +6,13 @@
 
 -define(eq(Expected, Actual), ?proplists_eq(Expected, Actual)).
 -define(TESTED, docsh_edoc).
--define(EXAMPLE, edoc_example).
 
 all() ->
-    [edoc_to_internal].
+    [edoc_to_internal,
+     edoc_format].
 
 edoc_to_internal(_) ->
-    File = source_file(?EXAMPLE),
+    File = source_file(edoc_example),
     ct:pal("~p", [File]),
     ?eq([{module, [{name, edoc_example},
                    {description, <<"Top-level module doc.">>}]},
@@ -22,6 +22,37 @@ edoc_to_internal(_) ->
                      {label, <<"f-0">>},
                      {description, <<"Doc for f/0.">>}]}],
         ?TESTED:to_internal(File)).
+
+edoc_format(_) ->
+    File = source_file(edoc_example2),
+    D = function_description(g, ?TESTED:to_internal(File)),
+    ct:pal("~s", [D]),
+    ?eq([<<"g() returns a more complex value,\n"
+           "while its documentation uses more complex markup.\n"
+           "\n"
+           "  Why?\n"
+           "  To test the documentation extraction and formatting process.\n"
+           "\n"
+           "  Any other reason?\n"
+           "  Not really.\n"
+           "\n"
+           "  1. Some\n"
+           "  2. Random\n"
+           "  3. Items\n"
+           "\n"
+           "  - One\n"
+           "  - Two\n"
+           "  - Three:\n"
+           "      - a\n"
+           "      - b\n"
+           "      - c\n">>],
+        [D]).
+
+function_description(F, Docs) ->
+    %% TODO: grrr... the internal format sucks
+    [Function] = proplists:get_all_values(function, Docs),
+    [{name,F},_,_,_,{description, D}] = Function,
+    D.
 
 source_file(Mod) ->
     proplists:get_value(source, Mod:module_info(compile)).
