39efe76f58cf59daf1b6dc633be8d49c2fa819f0
Radek Szymczyszyn <lavrin@gmail.com>
2016-01-31 14:37:29 +0100

    Fix one of the snippets

--- snippets.erl
+++ snippets.erl
@@ -2,9 +2,11 @@ P = "/home/erszcz/apps/elixir/1.2.1/bin/../lib/elixir/ebin/Elixir.Agent.beam".
 {ok, {_, C}} = beam_lib:chunks(P, ["ExDc"]).
 [{_, B}] = C.
 Docs = binary_to_term(B).
-{_ModLineNo, ModDoc} = proplists:get_value(moduledoc, element(2, DocsV1)).
-FunDocs = proplists:get_value(docs, element(2, DocsV1)).
+{_ModLineNo, ModDoc} = proplists:get_value(moduledoc, element(2, Docs)).
+FunDocs = proplists:get_value(docs, element(2, Docs)).
 {{update,3}, _FunLineNo, _Type, _Something, FunDoc} = lists:keyfind({update,3}, 1, FunDocs).
+ModDoc.
+FunDoc.
 
 S3 = "/home/erszcz/work/lavrin/docsh/test/edoc_example.erl".
 docsh_edoc:to_internal(S3).
