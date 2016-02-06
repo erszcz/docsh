2fb15356ef1b3db5a33acc9d2ceb2a997c3e9d2a
Radek Szymczyszyn <lavrin@gmail.com>
2016-02-06 14:48:38 +0100

    wip: Format leading / trailing separators differently

--- src/erl_types_prettypr_fmt.erl
+++ src/erl_types_prettypr_fmt.erl
@@ -7,18 +7,30 @@ comma_sequence(Types, RecDict) ->
                 true -> text("_");
                 false -> erl_types_fork:t_to_string(?MODULE, T, RecDict)
             end || T <- Types],
-    join(List, text(",")).
+    join(List, text(","), trailing).
 
 union_sequence(Types, RecDict) ->
     List = [erl_types_fork:t_to_string(?MODULE, T, RecDict) || T <- Types],
-    join(List, text(" | ")).
+    join(List, text("|"), leading).
 
 flat_format(F, S) ->
     text(lists:flatten(io_lib:format(F, S))).
 
-join([], _Sep) -> prettypr:empty();
-join([H], _Sep) -> H;
-join([H|T], Sep) -> prettypr:sep([prettypr:beside(H, text(Sep)), join(T, Sep)]).
+join(List, Sep) -> join_t(List, Sep).
+
+join(List, Sep, SepPosition) ->
+    case SepPosition of
+        trailing -> join_t(List, Sep);
+        leading  -> join_l(List, Sep)
+    end.
+
+join_t([], _Sep) -> prettypr:empty();
+join_t([H], _Sep) -> H;
+join_t([H|T], Sep) -> prettypr:sep([prettypr:beside(H, Sep), join_t(T, Sep)]).
+
+join_l([], _Sep) -> prettypr:empty();
+join_l([H], _Sep) -> H;
+join_l([H|T], Sep) -> prettypr:follow(H, prettypr:follow(text(Sep), join_l(T, Sep), 0), 0).
 
 text({text, _} = T) -> T;
 text([]) -> prettypr:empty();
