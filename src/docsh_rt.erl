4711fcb56849055a0d3b60e154208307d9ab5542
Radek Szymczyszyn <lavrin@gmail.com>
2016-02-14 00:48:43 +0100

    wip: print types

--- src/docsh_rt.erl
+++ src/docsh_rt.erl
@@ -1,24 +1,24 @@
 -module(docsh_rt).
 -compile([debug_info,
           {inline, [guard_no_docs/2,
-                    guard_not_supported/2]}]).
+                    guard_not_supported/2,
+                    types/1]}]).
 
 -export([h/1,
          h/3]).
 
 -import(docsh_lib, [print/2]).
 
+-define(a2b(A), atom_to_binary(A, utf8)).
+-define(i2b(I), integer_to_binary(I)).
 -define(il2b(IOList), iolist_to_binary(IOList)).
 
 h(Mod) ->
     F = fun (Docs) ->
                 {_, ModDoc} = proplists:get_value(moduledoc, Docs),
-                Types = proplists:get_value(types, Docs),
                 io_lib:format("## Description~n~n~s~n"
                               "## Types~n~n~s~n",
-                              [ModDoc,
-                               [ io_lib:format("  ~p :: ~s~n", [Name, Desc])
-                                 || {Name, Desc} <- Types ]])
+                              [ModDoc, types(Docs)])
         end,
     guard_no_docs(Mod, F).
 
@@ -48,3 +48,8 @@ guard_not_supported(Fun, {elixir_docs_v1, Docs}) ->
     Fun(Docs);
 guard_not_supported(_, _) ->
     <<"Documentation format not supported">>.
+
+types(Docs) ->
+    Types = proplists:get_value(types, Docs),
+    [ [<<"-type ">>, ?a2b(Name), $/, ?i2b(Arity), <<" :: ">>, Desc, $\n]
+      || {{Name, Arity}, Desc} <- Types ].
