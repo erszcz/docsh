acb2d2c71eb8ef738964a3a5c78cbebb4b7fed20
Radek Szymczyszyn <lavrin@gmail.com>
2016-03-09 21:11:58 +0100

    Move conversion code to docsh_lib

--- src/docsh_lib.erl
+++ src/docsh_lib.erl
@@ -5,7 +5,8 @@
          get/2, get/3,
          debug/3,
          print/2, print/3,
-         join/2]).
+         join/2,
+         process_beam/1]).
 
 -type k() :: any().
 -type v() :: any().
@@ -80,3 +81,65 @@ beam_diff(BEAM1, BEAM2) ->
     Keys2 = [ K || {K, _} <- Chunks2 ],
     [{BEAM1, Name1, Keys1 -- Keys2},
      {BEAM2, Name2, Keys2 -- Keys1}].
+
+process_beam(BEAMFile) ->
+    case {has_exdc(BEAMFile),
+          get_debug_info(BEAMFile),
+          get_source_file(BEAMFile)}
+    of
+        {true, _, _} ->
+            error("ExDc already present", [BEAMFile]);
+        {false, {ok, Abst}, _} ->
+            rebuild(BEAMFile, [exdc({abst, Abst})]);
+        %% TODO exdc from source file
+        %{false, _, {ok, File}} ->
+        %    rebuild(BEAMFile, [exdc({source, File})]);
+        _ ->
+            error("neither debug_info nor .erl available", [BEAMFile])
+    end.
+
+has_exdc(BEAMFile) ->
+    {ok, _, Chunks} = beam_lib:all_chunks(BEAMFile),
+    case catch ([ throw(true) || {"ExDc", _} <- Chunks ]) of
+        true -> true;
+        _    -> false
+    end.
+
+-spec get_debug_info(file:filename()) -> binary().
+get_debug_info(BEAMFile) ->
+    case beam_lib:chunks(BEAMFile, ["Abst"]) of
+        {ok, {_Module, [{"Abst", Abst}]}} -> {ok, Abst};
+        _ -> false
+    end.
+
+-spec get_source_file(file:filename()) -> file:filename().
+get_source_file(BEAMFile) ->
+    try get_source((modname(BEAMFile)):module_info(compile)) of
+        File when is_list(File) ->
+            case filelib:is_regular(File) of
+                true -> {ok, File};
+                _ -> false
+            end
+    catch _:_ -> false end.
+
+-spec modname(string()) -> module().
+modname(BEAMFile) ->
+    Base = filename:basename(BEAMFile, ".beam"),
+    list_to_atom(Base).
+
+get_source(CompileInfo) ->
+    {source, File} = lists:keyfind(source, 1, CompileInfo),
+    File.
+
+exdc({abst, BAbst}) ->
+    {raw_abstract_v1, Abst} = binary_to_term(BAbst),
+    FromMods = [docsh_edoc, docsh_syntax],
+    ToMod = docsh_elixir_docs_v1,
+    ExDc = convert(FromMods, ToMod, Abst),
+    {"ExDc", term_to_binary(ExDc)}.
+%% TODO exdc from source file
+%exdc({source, File})) ->
+
+rebuild(BEAMFile, NewChunks) ->
+    {ok, _, OldChunks} = beam_lib:all_chunks(BEAMFile),
+    {ok, _NewBEAM} = beam_lib:build_module(OldChunks ++ NewChunks).
