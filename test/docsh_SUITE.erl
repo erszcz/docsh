412bf4c94f66ccd2a6e04b75e6862491759a11ef
Radek Szymczyszyn <lavrin@gmail.com>
2016-04-02 16:23:32 +0200

    Generate docs from .beam's corresponding source file

--- test/docsh_SUITE.erl
+++ test/docsh_SUITE.erl
@@ -11,8 +11,10 @@
 all() ->
     [edoc_example_has_helpers,
      edoc_example_has_docs_from_debug_info,
+     edoc_example_has_docs_from_source,
      recon_has_helpers,
-     recon_has_docs_from_debug_info].
+     recon_has_docs_from_debug_info,
+     recon_has_docs_from_source].
 
 %%
 %% Tests
@@ -23,14 +25,20 @@ edoc_example_has_helpers(C) ->
     h2(C, edoc_example).
 
 edoc_example_has_docs_from_debug_info(C) ->
-    ?assert(has_docs(docsh_transform_in_memory(edoc_example))).
+    module_has_docs_from_debug_info(C, edoc_example).
+
+edoc_example_has_docs_from_source(C) ->
+    module_has_docs_from_source(C, edoc_example).
 
 recon_has_helpers(C) ->
     h0(C, recon),
     h2(C, recon).
 
 recon_has_docs_from_debug_info(C) ->
-    ?assert(has_docs(docsh_transform_in_memory(recon))).
+    module_has_docs_from_debug_info(C, recon).
+
+recon_has_docs_from_source(C) ->
+    module_has_docs_from_source(C, recon).
 
 %%
 %% Helpers
@@ -47,11 +55,45 @@ h2(_, Module) ->
     {Fun, Arity} = hd(Module:module_info(exports) -- [{h,0}, {h,2}]),
     ?eq(ok, Module:h(Fun, Arity)).
 
-docsh_transform_in_memory(Mod) ->
-    {module, Mod} = code:ensure_loaded(Mod),
+module_has_docs_from_debug_info(_, Mod) ->
+    %% given
     File = code:which(Mod),
+    ?assert(has_debug_info(File)),
+    %% when
+    BMod = docsh_transform_in_memory(File),
+    %% then
+    ?assert(has_docs(BMod)).
+
+module_has_docs_from_source(_, Mod) ->
+    %% given
+    File = code:which(Mod),
+    {ok, NoDebugInfoBMod} = strip_debug_info(File),
+    ?assert(not has_debug_info(NoDebugInfoBMod)),
+    %% when
+    BMod = docsh_transform_in_memory(NoDebugInfoBMod),
+    %% then
+    ?assert(has_docs(BMod)).
+
+docsh_transform_in_memory(File) ->
     {ok, NewBEAM} = docsh_lib:process_beam(File),
     NewBEAM.
 
 has_docs(Mod) ->
     docsh_lib:has_exdc(Mod).
+
+has_debug_info(Mod) ->
+    case docsh_lib:get_debug_info(Mod) of
+        {ok, _Abst} -> true;
+        false -> false
+    end.
+
+has_source(Mod) ->
+    case docsh_lib:get_source_file(Mod) of
+        {ok, _} -> true;
+        false -> false
+    end.
+
+strip_debug_info(BEAMFile) ->
+    {ok, _, Chunks} = beam_lib:all_chunks(BEAMFile),
+    NewChunks = lists:keystore("Abst", 1, Chunks, {"Abst", <<>>}),
+    {ok, _NewBEAM} = beam_lib:build_module(NewChunks).
