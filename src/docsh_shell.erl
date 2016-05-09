985e8ad88c32b8279f5e8800139949ff8c7416ae
Radek Szymczyszyn <lavrin@gmail.com>
2016-05-10 00:02:50 +0200

    Provide docs at runtime - no compile time preparation needed
    Given some prerequisites:
    
    - docsh available in Erlang code path (e.g. use `rebar3 shell`)
    - proper local paths to source files in `.beam` files
      (needs one manual step when installing with kerl/from a tarball)
    - OR debug info present in the .beam files
    
    It's possible to invoke h/1,3 from shell on OTP (or any other) modules:
    
        > h(fun lists:keyfind/3).
        -spec keyfind(Key, N, TupleList) -> Tuple | false
                         when
                             Key :: term(),
                             N :: pos_integer(),
                             TupleList :: [Tuple],
                             Tuple :: tuple().
    
        undefined
        ok
        >
    
    Pretty printing is still pretty ugly,
    but that's what Erlang provides in the standard library.

--- src/docsh_shell.erl
+++ src/docsh_shell.erl
@@ -5,34 +5,61 @@
 -spec h(fun() | module()) -> ok.
 h(Fun) when is_function(Fun) ->
     {M, F, A} = erlang:fun_info_mfa(Fun),
-    h(M, F, A);
+    h(M, [F, A]);
 
 h(M) when is_atom(M) ->
-    BEAMFile = code:which(M),
-    case docsh_lib:has_exdc(BEAMFile) of
-        true -> M:h();
-        false ->
-            reload_with_exdc(M, BEAMFile),
-            docsh_embeddable:h(M)
-    end.
+    h(M, []).
 
 h(M, F, A) ->
-    BEAMFile = code:which(M),
-    case docsh_lib:has_exdc(BEAMFile) of
-        true -> M:h(F, A);
-        false ->
-            reload_with_exdc(M, BEAMFile),
-            docsh_embeddable:h(M)
+    h(M, [F, A]).
+
+h(M, FunArity) when is_list(FunArity) ->
+    case docsh_beam:from_loadable_module(M) of
+        {error, R} ->
+            error(R, [M, FunArity]);
+        {ok, B} ->
+            case docsh_lib:has_exdc(docsh_beam:beam_file(B)) of
+                true ->
+                    %% TODO: We assume that having ExDc chunk implies also having the docsh support code.
+                    %%       This definitely won't be true for Elixir .beam files...
+                    %%       Check with erlang:function_exported/1,2 accordingly.
+                    %erlang:apply(M, h, FunArity);
+                    erlang:apply(docsh_embeddable, h, [M] ++ FunArity);
+                false ->
+                    {ok, NewB} = cached_or_rebuilt(B, cache_dir()),
+                    reload(NewB),
+                    h(M, FunArity)
+            end
     end.
 
-reload_with_exdc(M, BEAMFile) ->
-    {ok, NewBEAM} = docsh_lib:process_beam(BEAMFile),
-    unstick_module(M),
-    {module, M} = code:load_binary(M, BEAMFile, NewBEAM),
-    stick_module(M).
+-spec cached_or_rebuilt(docsh_beam:t(), file:name()) -> {ok, docsh_beam:t()}.
+cached_or_rebuilt(Beam, CacheDir) ->
+    %% TODO: find the module in cache, don't rebuild every time
+    {ok, _RebuiltB} = rebuild(Beam, CacheDir).
 
-%% Stolen from meck:
-%% https://github.com/eproxus/meck/blob/8902e5c9cdc5b3fca38cee830daaa15d1566167f/src/meck_proc.erl#L557
+cache_dir() ->
+    case {os:getenv("XDG_CACHE_HOME"), os:getenv("HOME")} of
+        {false, false} -> error(no_cache_dir);
+        {false, Home} -> filename:join([Home, ".docsh"]);
+        {XDGCache, _} -> filename:join([XDGCache, "docsh"])
+    end.
+
+reload(Beam) ->
+    BEAMFile = docsh_beam:beam_file(Beam),
+    Path = filename:join([filename:dirname(BEAMFile),
+                          filename:basename(BEAMFile, ".beam")]),
+    unstick_module(docsh_beam:name(Beam)),
+    {module, _} = code:load_abs(Path),
+    stick_module(docsh_beam:name(Beam)),
+    ok.
+
+rebuild(B, CacheDir) ->
+    BEAMFile = docsh_beam:beam_file(B),
+    {ok, NewBEAM} = docsh_lib:process_beam(BEAMFile),
+    NewBEAMFile = filename:join([CacheDir, filename:basename(BEAMFile)]),
+    ok = file:write_file(NewBEAMFile, NewBEAM),
+    docsh_beam:from_beam_file(NewBEAMFile).
+
 unstick_module(Module) -> unstick_module(Module, code:is_sticky(Module)).
 
 unstick_module(Module, true) -> code:unstick_mod(Module);
