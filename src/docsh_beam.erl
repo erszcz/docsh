fecb828845624a6b6de963d76606e26f1a58c1ed
Radek Szymczyszyn <lavrin@gmail.com>
2016-05-09 22:39:42 +0200

    Add docsh_beam:name/1

--- src/docsh_beam.erl
+++ src/docsh_beam.erl
@@ -3,15 +3,17 @@
 -export([from_beam_file/1,
          from_loadable_module/1,
 
+         name/1,
          abst/1, abst/2,
          beam_file/1, beam_file/2,
          source_file/1, source_file/2]).
 
 -export_type([t/0]).
 
--record(docsh_beam, {abst, beam_file, source_file}).
+-record(docsh_beam, {name, abst, beam_file, source_file}).
 
--opaque t() :: #docsh_beam{%% Abst chunk which may or may not be present in a loaded module.
+-opaque t() :: #docsh_beam{name :: module(),
+                           %% Abst chunk which may or may not be present in a loaded module.
                            abst :: [erl_parse:abstract_form()] | false,
                            %% `beam_file` has to be defined and is a file name.
                            %% TODO: Support dynamically compiled or network-loaded modules later.
@@ -31,16 +33,20 @@ from_loadable_module(Mod) ->
                E =:= preloaded ->
             {error, {no_beam_file, E}};
         BEAMFile ->
-            {ok, #docsh_beam{abst = debug_info(BEAMFile),
+            {ok, #docsh_beam{name = Mod,
+                             abst = debug_info(BEAMFile),
                              beam_file = BEAMFile,
                              source_file = bind(docsh_lib:get_source_file(BEAMFile))}}
     end.
 
 -spec from_beam_file(file:filename()) -> {ok, t()} | {error, any()}.
 from_beam_file(BEAMFile) ->
-    {ok, #docsh_beam{abst = debug_info(BEAMFile),
+    {ok, #docsh_beam{name = beam_name(BEAMFile),
+                     abst = debug_info(BEAMFile),
                      beam_file = BEAMFile,
                      source_file = bind(docsh_lib:get_source_file(BEAMFile))}}.
+
+name(DS) -> DS#docsh_beam.name.
 
 abst(DS) -> DS#docsh_beam.abst.
 
@@ -63,6 +69,10 @@ source_file(DS, source_file) ->
 
 bind({ok, V}) -> V;
 bind(false)   -> false.
+
+beam_name(BEAMFile) ->
+    {ok, N, _} = beam_lib:all_chunks(BEAMFile),
+    N.
 
 debug_info(BEAMFile) ->
     case bind(docsh_lib:get_debug_info(BEAMFile)) of
