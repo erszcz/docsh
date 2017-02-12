880b2583a5e0a6b8ad5a946396ef07873487523c
Radek Szymczyszyn <lavrin@gmail.com>
2017-02-12 11:15:47 +0100

    Fix xref_SUITE

--- test/xref_SUITE.erl
+++ test/xref_SUITE.erl
@@ -1,5 +1,4 @@
 -module(xref_SUITE).
--author('elbrujohalcon@inaka.net').
 
 -export([all/0]).
 -export([xref/1]).
@@ -9,9 +8,19 @@ all() -> [xref].
 
 -spec xref(lsl_test_utils:config()) -> {comment, []}.
 xref(_Config) ->
-  Dirs = [filename:absname("../../ebin")],
-  [] = xref_runner:check(undefined_function_calls, #{dirs => Dirs}),
-  [] = xref_runner:check(locals_not_used, #{dirs => Dirs}),
-  [] = xref_runner:check(deprecated_function_calls, #{dirs => Dirs}),
-  [] = xref_runner:check(deprecated_functions, #{dirs => Dirs}),
-{comment, ""}.
+    Dirs = [filename:join(code:lib_dir(docsh), "ebin")],
+    [] = filter_out_rebar3_as_dep( xref_runner:check(undefined_function_calls, #{dirs => Dirs}) ),
+    [] = xref_runner:check(locals_not_used, #{dirs => Dirs}),
+    [] = xref_runner:check(deprecated_function_calls, #{dirs => Dirs}),
+    [] = xref_runner:check(deprecated_functions, #{dirs => Dirs}),
+    {comment, ""}.
+
+filter_out_rebar3_as_dep(Warnings) ->
+    F = fun (#{target := {TargetMod, _, _}}) ->
+                case atom_to_list(TargetMod) of
+                    "rebar" ++ _ -> false;
+                    "providers" ++ _ -> false;
+                    _ -> true
+                end
+        end,
+    lists:filter(F, Warnings).
