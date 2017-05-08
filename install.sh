5f4aaf240b1ea39fac38a660dc673349abc6c176
Radek Szymczyszyn <lavrin@gmail.com>
2017-05-08 18:48:52 +0200

    [skip ci] Show on startup how to start using docsh

--- install.sh
+++ install.sh
@@ -29,7 +29,9 @@ read -r -d '' HOME_ERLANG_CONTENT <<EOF
 proplists:is_defined(noshell, init:get_arguments()) == false andalso begin
     DocshBase = "$DOCSH_BASE",
     code:add_path(DocshBase ++ "/_build/default/lib/docsh/ebin"),
-    io:format("Enabled docsh from: ~s\n", [DocshBase])
+    io:format("Enabled docsh from: ~s\n"
+              "Call h(docsh) for interactive help.\n\n",
+              [DocshBase])
 end.
 code:load_abs(os:getenv("HOME") ++ "/.erlang.d/user_default").
 EOF
