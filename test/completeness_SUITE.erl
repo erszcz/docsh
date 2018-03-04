1230ccb59740a8c1e8860095cf3fb10af4cb9e74
Radek Szymczyszyn <lavrin@gmail.com>
2018-03-04 16:43:59 +0100

    Gather some stats about EDoc availability per app

--- test/completeness_SUITE.erl
+++ test/completeness_SUITE.erl
@@ -13,28 +13,36 @@ sanity_check(_) -> ok.
 
 test1(_) ->
     %put(sh_log, true),
-    ct:pal("app_docs: ~p", [app_docs(stdlib)]),
+    Stats = [ app_stats(app_sources(App)) || App <- apps() ],
+    ct:pal("test1: ~p", [Stats]),
     ct:fail("intentional").
 
 apps() ->
     [
+     kernel,
      stdlib
     ].
 
-app_docs(App) ->
+app_sources(App) ->
     load_application(App),
     {ok, Modules} = application:get_key(App, modules),
-    app_modules_docs(App, Modules).
+    ModulesSources = app_modules_sources(App, Modules),
+    {App, ModulesSources}.
 
-app_modules_docs(App, Modules) ->
-    ModulesBeamsSources = [ begin
-                                {ok, B} = docsh_beam:from_loadable_module(M),
-                                Source = docsh_beam:source_file(B),
-                                case has_edoc_comments(Source) of
-                                    true -> {has_edoc, M, B, Source};
-                                    false -> {no_edoc, M, B, Source}
-                                end
-                            end || M <- Modules ].
+app_modules_sources(_App, Modules) ->
+    [ begin
+          {ok, B} = docsh_beam:from_loadable_module(M),
+          {M, docsh_beam:source_file(B)}
+      end || M <- Modules ].
+
+app_stats({App, ModulesSources}) ->
+    WithEdocFlag = [ case has_edoc_comments(Source) of
+                         true -> {M, has_edoc, Source};
+                         false -> {M, no_edoc, Source}
+                     end || {M, Source} <- ModulesSources ],
+    Stats = docsh_lib:group_by(fun ({_,HasEdoc,_}) -> HasEdoc end,
+                               WithEdocFlag),
+    {App, [ {Feature, length(Items), Items} || {Feature, Items} <- dict:to_list(Stats) ]}.
 
 load_application(App) ->
     case application:load(App) of
@@ -44,6 +52,7 @@ load_application(App) ->
     end.
 
 has_edoc_comments(SourceFile) ->
+    %% It's **so** unlikely that a file is EDoc-documented, but does not use the @doc tag.
     case sh(["grep -F @doc ", SourceFile, " > /dev/null"], [dont_fail]) of
         {done, 0, _} -> true;
         {done, _, _} -> false
