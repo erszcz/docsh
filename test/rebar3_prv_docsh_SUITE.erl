77b5994e2e06f9cbbe7de67f27a9152ba8bd9a07
Radek Szymczyszyn <lavrin@gmail.com>
2018-07-18 18:29:37 +0200

    Test that rebar3_prv_docsh_compiles_in_the_docs_chunk

--- test/rebar3_prv_docsh_SUITE.erl
+++ test/rebar3_prv_docsh_SUITE.erl
@@ -16,6 +16,9 @@ preconditions() ->
 end_per_suite(_Config) ->
     ok.
 
+all() ->
+    [rebar3_prv_docsh_compiles_in_the_docs_chunk].
+
 %%
 %% Config
 %%
@@ -27,7 +30,50 @@ recon_repo() ->
 %% Tests
 %%
 
+rebar3_prv_docsh_compiles_in_the_docs_chunk(_) ->
+    put(sh_log, true),
+    %% given
+    AppName = "recon",
+    sh(clone(recon_repo())),
+    %% when
+    {ok, ProjectDir} = compile(AppName),
+    %% then all modules have the "Docs" chunk
+    {ok, Modules} = app_modules(AppName, ProjectDir),
+    ModuleDocs = [ begin
+                       %% Not using docsh here so we're 100% sure it doesn't make docs_v1 on demand.
+                       BeamFile = code:which(M),
+                       {ok, {_Mod, [{"Docs", BDocs}]}} = beam_lib:chunks(BeamFile, ["Docs"]),
+                       {ok, erlang:binary_to_term(BDocs)}
+                   end || M <- Modules ],
+    ct:pal("~s module docs:\n~p", [AppName, ModuleDocs]),
+    [ok, ok, ok, ok] = [ element(1, MD) || MD <- ModuleDocs ],
+    ok.
 
 %%
 %% Helpers
 %%
+
+quote(Text) ->
+    ["\"", Text, "\""].
+
+clone(Repo) ->
+    ["git clone ", Repo].
+
+compile(Project) ->
+    {ok, Dir} = file:get_cwd(),
+    ProjectDir = filename:join([Dir, Project]),
+    try
+        ok = file:set_cwd(ProjectDir),
+        rebar_agent:start_link(rebar_state:new()),
+        r3:compile(),
+        {ok, ProjectDir}
+    after
+        ok = file:set_cwd(Dir)
+    end.
+
+app_modules(AppName, ProjectDir) ->
+    AppFile = filename:join([ProjectDir, "_build", "default", "lib", AppName, "ebin", AppName ++ ".app"]),
+    {ok, AppSpec} = file:consult(AppFile),
+    [{application, recon, AppProps}] = AppSpec,
+    {modules, Modules} = lists:keyfind(modules, 1, AppProps),
+    {ok, Modules}.
