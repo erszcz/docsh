92de955c31f06f974987096899e34d0a968c979d
Radek Szymczyszyn <lavrin@gmail.com>
2019-05-13 15:23:51 +0200

    [skip ci] Fix whitespace

--- src/docsh_stacktrace.hrl
+++ src/docsh_stacktrace.hrl
@@ -2,10 +2,10 @@
 -define(DOCSH_STACKTRACE_H, true).
 
 -ifdef(OTP_RELEASE).
-	%% For some time this clause will be implicit, since OTP_RELEASE was introduced in 21.
-	-if(?OTP_RELEASE >= 21).
-    -define(STACKTRACE(Type, Reason, Stacktrace), Type:Reason:Stacktrace ->).
-	-endif.
+    %% For some time this clause will be implicit, since OTP_RELEASE was introduced in 21.
+    -if(?OTP_RELEASE >= 21).
+        -define(STACKTRACE(Type, Reason, Stacktrace), Type:Reason:Stacktrace ->).
+    -endif.
 -else.
     %% OTP 20 or lower.
     -define(STACKTRACE(Type, Reason, Stacktrace), Type:Reason -> Stacktrace = erlang:get_stacktrace(), ).
