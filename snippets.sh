3683afb570b9a2e7cadb536e9c2787ddae0a555c
Radek Szymczyszyn <lavrin@gmail.com>
2019-11-02 12:20:22 +0100

    Save snippets

--- snippets.sh
+++ snippets.sh
@@ -1,3 +1,5 @@
 ./rebar3 compile && ./rebar3 escriptize
 erlc +debug_info -pa _build/default/lib/docsh/ebin/ test/recon.erl 
 _build/default/bin/docsh transform recon.beam to recon.beam
+
+rm -rf doc && r3 compile && ERL_LIBS=/Users/erszcz/work/erszcz/docsh/_build/default/lib /Users/erszcz/work/wojtekmach/docs_chunks/docs_chunks -project && ex_doc docsh "0.7.1" _build/default/lib/docsh/ebin -m docsh
